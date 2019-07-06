c0b3f2414c1fa4d7b512cb4c96a09001ce61d5e0
Amir Perehodnik <43294802+sl4vkek@users.noreply.github.com>
2019-07-06 09:22:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Minecraft_Coordinates2NetherBlocks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num_org_x = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.num_org_z = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.num_dest_z = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.num_dest_x = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.num_org_x)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_org_z)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_dest_z)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_dest_x)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // num_org_x
+            // 
+            this.num_org_x.Location = new System.Drawing.Point(43, 136);
+            this.num_org_x.Name = "num_org_x";
+            this.num_org_x.Size = new System.Drawing.Size(120, 20);
+            this.num_org_x.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nachlieli CLM", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Origin nether portal coordinates (overworld):";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(13, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X:";
+            // 
+            // num_org_z
+            // 
+            this.num_org_z.Location = new System.Drawing.Point(43, 162);
+            this.num_org_z.Name = "num_org_z";
+            this.num_org_z.Size = new System.Drawing.Size(120, 20);
+            this.num_org_z.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(13, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Z:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(13, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Z:";
+            // 
+            // num_dest_z
+            // 
+            this.num_dest_z.Location = new System.Drawing.Point(43, 255);
+            this.num_dest_z.Name = "num_dest_z";
+            this.num_dest_z.Size = new System.Drawing.Size(120, 20);
+            this.num_dest_z.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label5.Location = new System.Drawing.Point(13, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "X:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nachlieli CLM", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(12, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(367, 26);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Destination coordinates (overworld):";
+            // 
+            // num_dest_x
+            // 
+            this.num_dest_x.Location = new System.Drawing.Point(43, 229);
+            this.num_dest_x.Name = "num_dest_x";
+            this.num_dest_x.Size = new System.Drawing.Size(120, 20);
+            this.num_dest_x.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Miriam Libre", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Estimate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Font = new System.Drawing.Font("Miriam CLM", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lbl_result.Location = new System.Drawing.Point(17, 352);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(0, 16);
+            this.lbl_result.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nachlieli CLM", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label7.Location = new System.Drawing.Point(8, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(466, 70);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Calculate blocks in the\r\n overworld to blocks in the nether";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 468);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.num_dest_z);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.num_dest_x);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.num_org_z);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.num_org_x);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.num_org_x)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_org_z)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_dest_z)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_dest_x)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown num_org_x;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown num_org_z;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown num_dest_z;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown num_dest_x;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Coordinates2NetherBlocks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            num_dest_x.Maximum = decimal.MaxValue;
+            num_dest_z.Maximum = decimal.MaxValue;
+            num_org_x.Maximum = decimal.MaxValue;
+            num_org_z.Maximum = decimal.MaxValue;
+            num_dest_x.Minimum = decimal.MaxValue * -1;
+            num_dest_z.Minimum = decimal.MaxValue * -1;
+            num_org_x.Minimum = decimal.MaxValue * -1;
+            num_org_z.Minimum = decimal.MaxValue * -1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            decimal num_blocks_nether_x = (num_dest_x.Value - num_org_x.Value) / 8;
+            decimal num_blocks_nether_z = (num_dest_z.Value - num_org_z.Value) / 8;
+            string msg = "You have to walk " + num_blocks_nether_x.ToString() + " Blocks in X,"
+                + "\n"
+                +" and " + num_blocks_nether_z + " Blocks in Z in the nether.";
+            lbl_result.Text = msg;
+        }
+    }
+}
